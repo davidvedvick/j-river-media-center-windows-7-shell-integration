e0bfa659a4963eda78adf07d5efc03341f101393
dvedvick@gmail.com <dvedvick@gmail.com@5c459bcf-3cc9-7dec-09e2-ff0c81b16b2c>
2013-09-04 03:54:53 +0000

    - Moved loading playlist to background thread  - Fixed changing playlists when server changes  - Switched to JSON.net for JSON serialization  - Re-organized 3rd Party libraries into the "lib" folder

---
+++ MC_Aero_Taskbar_Plugin/McAeroTaskbarSettings.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MC_Aero_Taskbar_Plugin
+{
+    public class McAeroTaskbarSettings
+    {
+        public bool EnableCoverArt;
+        public bool DisplayArtistTrackName;
+        public bool TrackProgress;
+        public bool PlaylistProgress;
+        public bool NoProgressTrack;
+        public Dictionary<string, HashSet<string>> _pinnedPlaylists;
+
+        public HashSet<string> GetPinnedPlaylists(string library)
+        {
+            if (_pinnedPlaylists == null) _pinnedPlaylists = new Dictionary<string, HashSet<string>>();
+            if (!_pinnedPlaylists.ContainsKey(library)) _pinnedPlaylists[library] = new HashSet<string>(StringComparer.Ordinal);
+            return _pinnedPlaylists[library];
+        }
+    }
+}
